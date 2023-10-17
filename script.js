87cc0e5caa87fee8465a74a233881b9f23c1318f
Wicher <wicherICT@hotmail.com>
2023-10-17 12:50:54 +0200

    wip Guess My Number 68-72

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// This method is used top select html elements and is similar to css selectors. # would work for id selection, for example
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉Correct Number!👌';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+// getting value    getter
+console.log(document.querySelector('.guess').value);
+// setting value    setter
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
