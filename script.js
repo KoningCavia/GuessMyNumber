e60b838faa9d7db4647861581784b0ab950936f4
Wicher <wicherICT@hotmail.com>
2023-10-18 17:43:58 +0200

    manipulating css styles

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ console.log(document.querySelector('.guess').value);
 */ // getal tussen 0 & 0.999. * 20 (getal tussen 0 & 19) + 1 getal tussen 1 & 20
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-// document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').textContent = secretNumber;
 
 // select element Check (which is a button), add a click listener with a method/function linked to it. (when check button is clicked perform the following method,)
 document.querySelector('.check').addEventListener('click', function () {
@@ -26,9 +26,18 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess, typeof guess);
 
   if (!guess) {
+    //When there is no input
     document.querySelector('.message').textContent = 'No Number😒';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number!👌';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too high, try again';
@@ -37,6 +46,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = 'You lost the game 😖';
     }
+
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too low, try again';
