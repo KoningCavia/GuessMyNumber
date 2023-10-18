2fae80f53d2dd075f5c0d68895c9b0802328bc3e
Wicher <wicherICT@hotmail.com>
2023-10-18 17:36:36 +0200

    implementing game logic

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 
-*/
+*/ // getal tussen 0 & 0.999. * 20 (getal tussen 0 & 19) + 1 getal tussen 1 & 20
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+// document.querySelector('.number').textContent = secretNumber;
 
 // select element Check (which is a button), add a click listener with a method/function linked to it. (when check button is clicked perform the following method,)
 document.querySelector('.check').addEventListener('click', function () {
@@ -23,6 +26,24 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '🙌 No Number😒';
+    document.querySelector('.message').textContent = 'No Number😒';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!👌';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high, try again';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😖';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too low, try again';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😖';
+    }
   }
 });
