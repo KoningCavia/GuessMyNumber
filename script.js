c9d80b117ae51c3c21d0d73a81ba57065e79d9f7
Wicher <wicherICT@hotmail.com>
2023-10-18 18:02:47 +0200

    Coding Challenge. reset the game

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 
 */ // getal tussen 0 & 0.999. * 20 (getal tussen 0 & 19) + 1 getal tussen 1 & 20
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
@@ -32,9 +32,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number!👌';
-
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
 
     //When guess is too high
@@ -57,4 +55,25 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game 😖';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  //reset score
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  //Reset secret number
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = secretNumber;
+
+  //Reset guess gield
+  document.querySelector('.guess').value = '';
+
+  //Reset message
+  document.querySelector('.message').textContent =
+    'You started a new game, start guessing!';
+
+  //Reset CSS
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
